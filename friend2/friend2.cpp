df103d75141d8fb16b239ad83a6b77963ca22fff
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-07 18:38:36 +0700

    ke 12

--- friend2/friend2.cpp
+++ friend2/friend2.cpp
@@ -11,3 +11,16 @@ public:
 	friend void setnama(mahasiswa& a, string);
 };
 
+void setnama(mahasiswa& a, string b)
+{
+	a.nama = b;
+	cout << a.nama;
+}
+
+int main()
+{
+	mahasiswa joko;
+	setnama(joko, "kairi kumar");
+	return 0;
+}
+
